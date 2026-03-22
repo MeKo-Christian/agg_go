c70837a302c70d1037e321479bef25be902f419f
Christian Budde <Christian.Budde@meko.de>
2026-03-22 22:08:23 +0100

    feat: Refactor image rendering to enhance control handling and streamline transformation logic

--- examples/core/intermediate/image1/main.go
+++ examples/core/intermediate/image1/main.go
@@ -18,74 +18,20 @@ import (
 	"github.com/MeKo-Christian/agg_go/internal/basics"
 	"github.com/MeKo-Christian/agg_go/internal/buffer"
 	"github.com/MeKo-Christian/agg_go/internal/color"
-	"github.com/MeKo-Christian/agg_go/internal/image"
-	"github.com/MeKo-Christian/agg_go/internal/path"
+	"github.com/MeKo-Christian/agg_go/internal/conv"
+	ctrlbase "github.com/MeKo-Christian/agg_go/internal/ctrl"
+	sliderctrl "github.com/MeKo-Christian/agg_go/internal/ctrl/slider"
 	"github.com/MeKo-Christian/agg_go/internal/pixfmt"
 	"github.com/MeKo-Christian/agg_go/internal/rasterizer"
 	"github.com/MeKo-Christian/agg_go/internal/renderer"
+	renscan "github.com/MeKo-Christian/agg_go/internal/renderer/scanline"
 	"github.com/MeKo-Christian/agg_go/internal/scanline"
+	"github.com/MeKo-Christian/agg_go/internal/shapes"
 	"github.com/MeKo-Christian/agg_go/internal/span"
 	"github.com/MeKo-Christian/agg_go/internal/transform"
 )
 
 const defaultImageName = "spheres"
-
-// imagePixFmt wraps a RenderingBufferU8 and implements image.PixelFormat.
-type imagePixFmt struct {
-	rbuf *buffer.RenderingBufferU8
-}
-
-func (p imagePixFmt) Width() int    { return p.rbuf.Width() }
-func (p imagePixFmt) Height() int   { return p.rbuf.Height() }
-func (p imagePixFmt) PixWidth() int { return 4 }
-func (p imagePixFmt) PixPtr(x, y int) []basics.Int8u {
-	row := buffer.RowU8(p.rbuf, y)
-	return row[x*4:]
-}
-
-// imageClipSource implements span.RGBASourceInterface for image access.
-type imageClipSource struct {
-	accessor *image.ImageAccessorClip[imagePixFmt]
-	ipf      *imagePixFmt
-}
-
-func (s *imageClipSource) Width() int                  { return s.ipf.Width() }
-func (s *imageClipSource) Height() int                 { return s.ipf.Height() }
-func (s *imageClipSource) ColorType() string           { return "RGBA8" }
-func (s *imageClipSource) OrderType() color.ColorOrder { return color.OrderRGBA }
-func (s *imageClipSource) Span(x, y, l int) []basics.Int8u {
-	return s.accessor.Span(x, y, l)
-}
-func (s *imageClipSource) NextX() []basics.Int8u { return s.accessor.NextX() }
-func (s *imageClipSource) NextY() []basics.Int8u { return s.accessor.NextY() }
-func (s *imageClipSource) RowPtr(y int) []basics.Int8u {
-	return s.ipf.PixPtr(0, y)
-}
-
-// spanGenAdapter wraps SpanImageFilterRGBABilinearClip for the render loop.
-type spanGenAdapter struct {
-	sg *span.SpanImageFilterRGBABilinearClip[*imageClipSource, *span.SpanInterpolatorLinear[*transform.TransAffine]]
-}
-
-func (a *spanGenAdapter) Prepare() {}
-func (a *spanGenAdapter) Generate(colors []color.RGBA8[color.Linear], x, y, length int) {
-	if length > len(colors) {
-		length = len(colors)
-	}
-	a.sg.Generate(colors[:length], x, y)
-}
-
-// rasScanlineAdapter adapts ScanlineU8 to rasterizer.ScanlineInterface.
-// pathSourceAdapter bridges PathStorageStl to rasterizer VertexSource.
-type pathSourceAdapter struct{ ps *path.PathStorageStl }
-
-func (a *pathSourceAdapter) Rewind(id uint32) { a.ps.Rewind(uint(id)) }
-func (a *pathSourceAdapter) Vertex(x, y *float64) uint32 {
-	vx, vy, cmd := a.ps.NextVertex()
-	*x = vx
-	*y = vy
-	return cmd
-}
 
 func loadPPMImage(filename string) (*agg.Image, error) {
 	data, err := os.ReadFile(filename)
@@ -170,37 +116,114 @@ func loadPPMImage(filename string) (*agg.Image, error) {
 	return agg.NewImage(buf, w, h, w*4), nil
 }
 
-func buildTransformedEllipsePath(canvasW, canvasH int, mtx *transform.TransAffine) *path.PathStorageStl {
-	initialW := float64(canvasW)
-	initialH := float64(canvasH)
-	r := initialW
-	if initialH-60.0 < r {
-		r = initialH - 60.0
-	}
-	rx := r*0.5 + 16.0
-	ry := r*0.5 + 16.0
-	cx := initialW*0.5 + 10.0
-	cy := initialH*0.5 + 30.0
-
-	ps := path.NewPathStorageStl()
-	const steps = 200
-	for i := 0; i <= steps; i++ {
-		a := 2 * math.Pi * float64(i) / float64(steps)
-		x := cx + rx*math.Cos(a)
-		y := cy + ry*math.Sin(a)
-		mtx.Transform(&x, &y)
-		if i == 0 {
-			ps.MoveTo(x, y)
-		} else {
-			ps.LineTo(x, y)
-		}
-	}
-	ps.ClosePolygon(basics.PathFlagsNone)
-	return ps
-}
-
 type demo struct {
 	srcImg *agg.Image
+	angle  *sliderctrl.SliderCtrl
+	scale  *sliderctrl.SliderCtrl
+	w, h   int
+}
+
+func newDemo(srcImg *agg.Image) *demo {
+	angle := sliderctrl.NewSliderCtrl(5, 5, 300, 12, false)
+	scale := sliderctrl.NewSliderCtrl(5, 5+15, 300, 12+15, false)
+
+	angle.SetLabel("Angle=%3.2f")
+	scale.SetLabel("Scale=%3.2f")
+	angle.SetRange(-180.0, 180.0)
+	angle.SetValue(0.0)
+	scale.SetRange(0.1, 5.0)
+	scale.SetValue(1.0)
+
+	return &demo{
+		srcImg: srcImg,
+		angle:  angle,
+		scale:  scale,
+		w:      srcImg.Width() + 20,
+		h:      srcImg.Height() + 40 + 20,
+	}
+}
+
+type ctrlVS struct {
+	ctrl ctrlbase.Ctrl[color.RGBA]
+}
+
+func (a *ctrlVS) Rewind(id uint32) { a.ctrl.Rewind(uint(id)) }
+func (a *ctrlVS) Vertex(x, y *float64) uint32 {
+	vx, vy, cmd := a.ctrl.Vertex()
+	*x, *y = vx, vy
+	return uint32(cmd)
+}
+
+func clampU8(v float64) uint8 {
+	if v <= 0 {
+		return 0
+	}
+	if v >= 1 {
+		return 255
+	}
+	return uint8(v*255.0 + 0.5)
+}
+
+func renderCtrl(
+	ras *rasterizer.RasterizerScanlineAA[int, rasterizer.RasConvInt, *rasterizer.RasterizerSlNoClip],
+	sl *scanline.ScanlineU8,
+	renBase *renderer.RendererBase[*pixfmt.PixFmtRGBA32[color.Linear], color.RGBA8[color.Linear]],
+	ctrl ctrlbase.Ctrl[color.RGBA],
+) {
+	for pathID := uint(0); pathID < ctrl.NumPaths(); pathID++ {
+		ras.Reset()
+		ras.AddPath(&ctrlVS{ctrl: ctrl}, uint32(pathID))
+		c := ctrl.Color(pathID)
+		renscan.RenderScanlinesAASolid(ras, sl, renBase, color.RGBA8[color.Linear]{
+			R: clampU8(c.R),
+			G: clampU8(c.G),
+			B: clampU8(c.B),
+			A: clampU8(c.A),
+		})
+	}
+}
+
+// imagePixFmt wraps a RenderingBufferU8 and implements span.RGBASourceInterface.
+type imagePixFmt struct {
+	rbuf *buffer.RenderingBufferU8
+}
+
+func (p *imagePixFmt) Width() int    { return p.rbuf.Width() }
+func (p *imagePixFmt) Height() int   { return p.rbuf.Height() }
+func (p *imagePixFmt) PixWidth() int { return 4 }
+func (p *imagePixFmt) PixPtr(x, y int) []basics.Int8u {
+	row := buffer.RowU8(p.rbuf, y)
+	return row[x*4:]
+}
+
+func (p *imagePixFmt) ColorType() string           { return "RGBA8" }
+func (p *imagePixFmt) OrderType() color.ColorOrder { return color.OrderRGBA }
+func (p *imagePixFmt) RowPtr(y int) []basics.Int8u { return p.PixPtr(0, y) }
+
+// Stub methods to satisfy RGBASourceInterface
+func (p *imagePixFmt) Span(x, y, length int) []basics.Int8u { return nil }
+func (p *imagePixFmt) NextX() []basics.Int8u                { return nil }
+func (p *imagePixFmt) NextY() []basics.Int8u                { return nil }
+
+type ellipseVS struct {
+	e *shapes.Ellipse
+}
+
+func (ev *ellipseVS) Rewind(id uint) { ev.e.Rewind(uint32(id)) }
+func (ev *ellipseVS) Vertex() (float64, float64, basics.PathCommand) {
+	var x, y float64
+	cmd := ev.e.Vertex(&x, &y)
+	return x, y, cmd
+}
+
+// spanGeneratorAdapter wraps SpanImageFilterRGBABilinearClip to satisfy renscan.SpanGeneratorInterface.
+type spanGeneratorAdapter struct {
+	sg *span.SpanImageFilterRGBABilinearClip[*imagePixFmt, *span.SpanInterpolatorLinear[*transform.TransAffine]]
+}
+
+func (a *spanGeneratorAdapter) Prepare() {}
+func (a *spanGeneratorAdapter) Generate(colors []color.RGBA8[color.Linear], x, y, length int) {
+	a.sg.Generate(colors[:length], x, y)
 }
 
 func (d *demo) Render(img *agg.Image) {
@@ -208,85 +231,103 @@ func (d *demo) Render(img *agg.Image) {
 		return
 	}
 
-	canvasW := img.Width()
-	canvasH := img.Height()
+	w, h := img.Width(), img.Height()
 
-	ctx := agg.NewContextForImage(img)
-	ctx.Clear(agg.RGBA(1, 1, 1, 1))
+	dstRbuf := buffer.NewRenderingBufferU8WithData(img.Data, w, h, img.Stride())
+	dstPixf := pixfmt.NewPixFmtRGBA32[color.Linear](dstRbuf)
+	dstPixfPre := pixfmt.NewPixFmtRGBA32PreLinear(dstRbuf)
+	renBase := renderer.NewRendererBaseWithPixfmt[*pixfmt.PixFmtRGBA32[color.Linear], color.RGBA8[color.Linear]](dstPixf)
+	renBasePre := renderer.NewRendererBaseWithPixfmt[*pixfmt.PixFmtRGBA32Pre[color.Linear], color.RGBA8[color.Linear]](dstPixfPre)
 
-	dstRbuf := buffer.NewRenderingBufferWithData[uint8](img.Data, img.Width(), img.Height(), img.Width()*4)
-	dstPixf := pixfmt.NewPixFmtRGBA32Pre[color.Linear](dstRbuf)
-	renBase := renderer.NewRendererBaseWithPixfmt[*pixfmt.PixFmtRGBA32Pre[color.Linear], color.RGBA8[color.Linear]](dstPixf)
-	alloc := span.NewSpanAllocator[color.RGBA8[color.Linear]]()
+	renBase.Clear(color.RGBA8[color.Linear]{R: 255, G: 255, B: 255, A: 255})
+
+	initW := float64(d.w)
+	initH := float64(d.h)
+
+	srcMtx := transform.NewTransAffine()
+	srcMtx.Translate(-initW/2-10, -initH/2-20-10)
+	srcMtx.Rotate(d.angle.Value() * math.Pi / 180.0)
+	srcMtx.Scale(d.scale.Value())
+	srcMtx.Translate(initW/2, initH/2+20)
+
+	imgMtx := transform.NewTransAffine()
+	imgMtx.Translate(-initW/2+10, -initH/2+20+10)
+	imgMtx.Rotate(d.angle.Value() * math.Pi / 180.0)
+	imgMtx.Scale(d.scale.Value())
+	imgMtx.Translate(initW/2, initH/2+20)
+	imgMtx.Invert()
+
+	imgRbuf := buffer.NewRenderingBufferU8WithData(d.srcImg.Data, d.srcImg.Width(), d.srcImg.Height(), d.srcImg.Width()*4)
+	ipf := &imagePixFmt{rbuf: imgRbuf}
+
+	// Bilinear filtered image generator
+	interp := span.NewSpanInterpolatorLinearDefault(imgMtx)
+	// C++: rgba_pre(0, 0.4, 0, 0.5) => (0, 102, 0, 128)
+	clipColor := color.RGBA8[color.Linear]{R: 0, G: 102, B: 0, A: 128}
+	sg := span.NewSpanImageFilterRGBABilinearClipWithParams[*imagePixFmt, *span.SpanInterpolatorLinear[*transform.TransAffine]](ipf, clipColor, interp)
+	sgAdapter := &spanGeneratorAdapter{sg: sg}
 
 	ras := rasterizer.NewRasterizerScanlineAA[int, rasterizer.RasConvInt, *rasterizer.RasterizerSlNoClip](
 		rasterizer.RasConvInt{}, rasterizer.NewRasterizerSlNoClip(),
 	)
 	sl := scanline.NewScanlineU8()
+	sa := span.NewSpanAllocator[color.RGBA8[color.Linear]]()
 
-	angle := 0.0
-	scale := 1.0
-	initialW := float64(canvasW)
-	initialH := float64(canvasH)
-
-	srcMtx := transform.NewTransAffine()
-	srcMtx.Translate(-initialW*0.5-10.0, -initialH*0.5-30.0)
-	srcMtx.Rotate(angle * math.Pi / 180.0)
-	srcMtx.Scale(scale)
-	srcMtx.Translate(initialW*0.5, initialH*0.5+20.0)
-
-	imgMtx := transform.NewTransAffine()
-	imgMtx.Translate(-initialW*0.5+10.0, -initialH*0.5+30.0)
-	imgMtx.Rotate(angle * math.Pi / 180.0)
-	imgMtx.Scale(scale)
-	imgMtx.Translate(initialW*0.5, initialH*0.5+20.0)
-	imgMtx.Invert()
-
-	imgRbuf := buffer.NewRenderingBufferU8()
-	imgRbuf.Attach(d.srcImg.Data, d.srcImg.Width(), d.srcImg.Height(), d.srcImg.Width()*4)
-	ipf := imagePixFmt{rbuf: imgRbuf}
-
-	// C++ clip color: rgba_pre(0, 0.4, 0, 0.5)
-	accessor := image.NewImageAccessorClip(&ipf, []basics.Int8u{0, 102, 0, 128})
-	src := &imageClipSource{accessor: accessor, ipf: &ipf}
-
-	bgRGBA := color.RGBA8[color.Linear]{R: 0, G: 102, B: 0, A: 128}
-	interp := span.NewSpanInterpolatorLinear[*transform.TransAffine](imgMtx, 8)
-	sg := span.NewSpanImageFilterRGBABilinearClipWithParams(src, bgRGBA, interp)
-	sgAdp := &spanGenAdapter{sg: sg}
-
-	clipPath := buildTransformedEllipsePath(canvasW, canvasH, srcMtx)
+	r := initW
+	if initH-60 < r {
+		r = initH - 60
+	}
+	ell := shapes.NewEllipseWithParams(initW/2+10, initH/2+20+10, r/2+16, r/2+16, 200, false)
+	tr := conv.NewConvTransform[conv.VertexSource, *transform.TransAffine](&ellipseVS{e: ell}, srcMtx)
 
 	ras.Reset()
-	ras.ClipBox(0, 0, float64(canvasW), float64(canvasH))
-	ras.AddPath(&pathSourceAdapter{ps: clipPath}, 0)
+	ras.AddPath(conv.NewRasterizerVertexSourceAdapter(tr), 0)
+	renscan.RenderScanlinesAA(ras, sl, renBasePre, sa, sgAdapter)
 
-	if ras.RewindScanlines() {
-		sl.Reset(ras.MinX(), ras.MaxX())
-		for ras.SweepScanline(sl) {
-			y := sl.Y()
-			for _, spanData := range sl.Spans() {
-				if spanData.Len <= 0 {
-					continue
-				}
-				colors := alloc.Allocate(int(spanData.Len))
-				sgAdp.Generate(colors, int(spanData.X), y, int(spanData.Len))
-				renBase.BlendColorHspan(int(spanData.X), y, int(spanData.Len), colors, spanData.Covers, basics.CoverFull)
-			}
+	renderCtrl(ras, sl, renBase, d.angle)
+	renderCtrl(ras, sl, renBase, d.scale)
+}
+
+func (d *demo) OnMouseDown(x, y int, btn lowlevelrunner.Buttons) bool {
+	fx, fy := float64(x), float64(y)
+	if btn.Left {
+		if d.angle.OnMouseButtonDown(fx, fy) || d.scale.OnMouseButtonDown(fx, fy) {
+			return true
 		}
 	}
+	return false
+}
+
+func (d *demo) OnMouseMove(x, y int, btn lowlevelrunner.Buttons) bool {
+	fx, fy := float64(x), float64(y)
+	if d.angle.OnMouseMove(fx, fy, btn.Left) || d.scale.OnMouseMove(fx, fy, btn.Left) {
+		return true
+	}
+	return false
+}
+
+func (d *demo) OnMouseUp(x, y int, btn lowlevelrunner.Buttons) bool {
+	fx, fy := float64(x), float64(y)
+	if d.angle.OnMouseButtonUp(fx, fy) || d.scale.OnMouseButtonUp(fx, fy) {
+		return true
+	}
+	return false
 }
 
 func main() {
+	// Paths to look for spheres.ppm
 	srcPath := filepath.Join("examples", "shared", "art", defaultImageName+".ppm")
+
 	srcImg, err := loadPPMImage(srcPath)
 	if err != nil {
 		panic(err)
 	}
 
+	d := newDemo(srcImg)
 	lowlevelrunner.Run(lowlevelrunner.Config{
-		Title:  "Image1",
-		Width:  340,
-		Height: 360,
-	}, &demo{srcImg: srcImg})
+		Title:  "AGG Example. Image Affine Transformations with filtering",
+		Width:  d.w,
+		Height: d.h,
+		FlipY:  true,
+	}, d)
 }
